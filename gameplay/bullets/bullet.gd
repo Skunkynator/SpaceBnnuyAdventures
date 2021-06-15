bb2728337e9d50ab60428f74e369ce0cc5a228f3
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2021-06-15 17:15:14 +0200

    add bullets

--- gameplay/bullets/bullet.gd
+++ gameplay/bullets/bullet.gd
@@ -2,17 +2,20 @@ extends Node2D
 class_name Bullet
 
 
+export var speed : float = 40
 export var damage : float = 4
 export var playerBullet : bool = true
-var direction: Vector2 = Vector2(0,1) setget set_direction,get_direction
+export var direction: Vector2 = Vector2(0,-1)
 
 
 func _process(delta):
-	position += direction * delta
+	position += direction * delta * speed
 
-func set_direction(dir: Vector2):
+
+func _on_bullet_screen_exited():
+	queue_free()
 	pass
 
 
-func get_direction():
-	return direction
+func _hit(damage: float):
+	queue_free()
