e4fe085e24f5310bad942ddde6efe559afa0320f
Skunkynator <43096109+Skunkynator@users.noreply.github.com>
2021-06-15 17:19:45 +0200

    ship overhaul

--- gameplay/ships/player.gd
+++ gameplay/ships/player.gd
@@ -3,18 +3,32 @@ class_name Player
 
 
 func _ready():
-	pass
+	GameController.player = self
 
 
 func _process(delta):
 	position += delta * GameInput.inputDirection * Speed
 	clampToScreen()
-	var instance : Node2D = projectile.instance()
-	instance.position = position
-	get_parent().add_child(instance)
+
+
+func _shoot_bullets():
+	var instance : Bullet
+	var offset : Vector2
+	while(health > 0):
+		while(GameInput._shoot_pressed_amount != 0):
+			_shoot()
+			yield(get_tree().create_timer(shootDelta),"timeout")
+		yield(GameInput,"shoot_pressed")
 
 
 func clampToScreen():
 	var screenSize = get_viewport_rect().size
 	position.x = clamp(position.x,Size.x,screenSize.x - Size.x)
 	position.y = clamp(position.y,Size.y,screenSize.y - Size.y)
+
+
+func _on_bullet_hit(bullet: Bullet):
+	if not bullet.playerBullet:
+		print_debug("owo :" + str(health))
+		health -= bullet.damage
+		bullet._hit(bullet.damage)
--- gameplay/ships/ship.gd
+++ gameplay/ships/ship.gd
@@ -4,19 +4,42 @@ class_name Ship
 
 export var health := 100.0
 export var Speed: float = 80.0
+export var shootDelta : float = 0.5
 export var Size: Vector2 = Vector2(16,16)
 export var projectile: PackedScene
 
 
 func _ready():
+	_shoot_bullets()
 	pass
 
 
 func _on_ship_area_entered(area):
 	if area.get_parent() is Bullet:
-		_onBulletHit(area)
+		_on_bullet_hit(area.get_parent())
 	pass
 
 
-func _onBulletHit(bullet: Bullet):
+func _on_bullet_hit(bullet: Bullet):
+	if bullet.playerBullet:
+		health -= bullet.damage
+		if health <= 0:
+			queue_free()
+			bullet._hit(bullet.damage + health)
+		else:
+			bullet._hit(bullet.damage)
+
+
+func _shoot_bullets():
+	while(health > 0):
+		_shoot()
+		yield(get_tree().create_timer(shootDelta),"timeout")
 	pass
+
+func _shoot():
+	var instance : Bullet
+	var offset : Vector2
+	instance = projectile.instance()
+	offset = instance.direction.normalized() * Size.length() / 3
+	instance.position = position + offset
+	get_parent().add_child(instance)
